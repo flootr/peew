5496013f3e753e25596a2b22669aa152cdfd7500
Markus Dobmann <md@noqq.eu>
2015-05-27 18:09:09 +0200

    Fix response example

--- examples/response.js
+++ examples/response.js
@@ -7,7 +7,7 @@ var inspect = require('util').inspect;
 var Parser = require('../lib').Parser;
 var xmlParser = new Parser();
 
-var xmlResponse = fs.readFileSync(path.resolve(__dirname, 'caldavResponse.xml'), 'utf8');
+var xmlResponse = fs.readFileSync(path.resolve(__dirname, 'response.xml'), 'utf8');
 
 xmlParser.parse(xmlResponse)
     .then(function (res) {
