ab2b1a175691b880b1577820056207b3cb7c64ca
Markus Dobmann <md@noqq.eu>
2015-05-29 21:31:18 +0200

    Rename parser to tree

--- test/lib/Tree.test.js
+++ test/lib/Tree.test.js
@@ -3,16 +3,16 @@
 var expect = require('chai').expect;
 
 var Element = require('../../lib').Element;
-var Parser = require('../../lib').Parser;
-var xmlParser = new Parser();
+var Tree = require('../../lib').Tree;
+var xmlTree = new Tree();
 
-describe('Parser', function () {
+describe('Tree', function () {
 
     describe('parse()', function () {
 
         it('should throw an error if an xml tag has a closed but no starting tag', function () {
             var xmlString = '<opening></missing></opening>';
-            return xmlParser.parse(xmlString)
+            return xmlTree.parse(xmlString)
                 .catch(function (err) {
                     expect(err).to.be.instanceof(Error);
                 });
@@ -22,7 +22,7 @@ describe('Parser', function () {
             var xmlString = '<opening src="happening"><happening></happening></opening>';
             var expectedObject = new Element('opening', {src: 'happening'}).addChildNode(new Element('happening')).root();
 
-            return xmlParser.parse(xmlString)
+            return xmlTree.parse(xmlString)
                 .then(function (res) {
                     expect(res).to.eql(expectedObject);
                 });
