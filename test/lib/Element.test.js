8ad18c44743368abe787fd031ebaf6207c4a8c80
Markus Dobmann <md@noqq.eu>
2015-12-03 16:58:32 +0100

    Apply linting rules

--- test/lib/Element.test.js
+++ test/lib/Element.test.js
@@ -1,116 +1,121 @@
 "use strict";
 
-var expect = require('chai').expect;
-var Element = require('../../lib').Element;
+/*eslint-disable max-nested-callbacks */
 
-describe('Element', function () {
+const expect = require("chai").expect;
+const Element = require("../../lib").Element;
 
-    var newElement;
+describe("Element", () => {
+    let newElement;
 
-    beforeEach(function () {
-        newElement = new Element('beautifulNewElement', {});
+    beforeEach(() => {
+        newElement = new Element("beautifulNewElement", {});
     });
 
-    describe('new Element()', function () {
-        it('should throw an error if there is no given name', function () {
-            var t;
-            try {
-                t = new Element();
-            } catch (error) {
-                expect(error).to.be.instanceof(Error);
-                expect(error.message).to.equal('Failed due to missing \'name\'');
-            }
+    describe("new Element()", () => {
+        it("should throw an error if there is no given name", () => {
+            expect(() => new Element()).to.throw(Error, "Failed due to missing \"name\"");
         });
 
-        it('should return an instance of Element', function () {
+        it("should return an instance of Element", () => {
             expect(newElement).to.be.instanceof(Element);
-            expect(newElement).to.contain.all.keys(['attrs', 'children', 'name', 'parent']);
+            expect(newElement).to.contain.all.keys(["attrs", "children", "name", "parent"]);
         });
     });
 
-    describe('setAttributes()', function () {
-        it('should set the proper attributes', function () {
-            newElement.setAttributes({'type': 'PseudoObject', 'charset': 'utf8'});
-            expect(newElement.attrs).to.contain.all.keys(['type', 'charset']);
-            expect(newElement.attrs.type).to.equal('PseudoObject');
-            expect(newElement.attrs.charset).to.equal('utf8');
+    describe("setAttributes()", () => {
+        it("should set the proper attributes", () => {
+            newElement.setAttributes({ type: "PseudoObject", charset: "utf8" });
+            expect(newElement.attrs).to.contain.all.keys(["type", "charset"]);
+            expect(newElement.attrs.type).to.equal("PseudoObject");
+            expect(newElement.attrs.charset).to.equal("utf8");
         });
     });
 
-    describe('root()', function () {
-        it('should return the correct root Element', function () {
-            var newSubElement = new Element('beautifulNewSubElement', {});
+    describe("root()", () => {
+        it("should return the correct root Element", () => {
+            const newSubElement = new Element("beautifulNewSubElement", {});
+
             newElement.addChildNode(newSubElement);
             expect(newElement.children).to.be.instanceof(Array);
             expect(newSubElement.root()).to.equal(newElement);
         });
     });
 
-    describe('parent()', function () {
-        it('should return the correct parent Element', function () {
-            var newSubElement = new Element('beautifulNewSubElement', {});
+    describe("parent()", () => {
+        it("should return the correct parent Element", () => {
+            const newSubElement = new Element("beautifulNewSubElement", {});
+
             newElement.addChildNode(newSubElement);
             expect(newSubElement.up()).to.equal(newElement);
             expect(newElement.up()).to.equal(newElement);
         });
     });
 
-    describe('addChildNode()', function () {
-        it('should set the correct number of child nodes', function () {
-            var elements = [];
-            for (var i = 0; i < 23; i++) {
-                elements.push(new Element('beautifulElementNo' + i, {}));
+    describe("addChildNode()", () => {
+        it("should set the correct number of child nodes", () => {
+            const elements = [];
+
+            for (let i = 0; i < 23; i++) {
+                elements.push(new Element("beautifulElementNo" + i, {}));
             }
-            elements.map(function (element) {
+
+            elements.map((element) => {
                 newElement.addChildNode(element);
             });
 
             expect(newElement.children).to.be.instanceof(Array);
             expect(newElement.children).to.have.length(23);
-            newElement.children.map(function (child) {
+            newElement.children.map((child) => {
                 expect(elements).to.contain(child);
             });
         });
     });
 
-    describe('treeFindAll()', function () {
-        it('should return the proper Elements', function () {
-            newElement.addChildNode(new Element('beautifulSubNode', {}));
-            newElement.find('beautifulSubNode').addChildNode(new Element('beautifulSubSubNode', {}));
-            var results = newElement.treeFindAll('beautifulSubSubNode');
+    describe("treeFindAll()", () => {
+        it("should return the proper Elements", () => {
+            newElement.addChildNode(new Element("beautifulSubNode", {}));
+            newElement.find("beautifulSubNode").addChildNode(new Element("beautifulSubSubNode", {}));
+
+            const results = newElement.treeFindAll("beautifulSubSubNode");
+
             expect(results).to.be.instanceof(Array);
             expect(results).to.have.length(1);
-            expect(results[0].name).to.eql('beautifulSubSubNode');
+            expect(results[0].name).to.eql("beautifulSubSubNode");
         });
     });
 
-    describe('keys()', function () {
-        it('should return the elements attribute names', function () {
-            var attributes = {
+    describe("keys()", () => {
+        it("should return the elements attribute names", () => {
+            const attributes = {
                 values: 3,
-                wow: 'hammer'
+                wow: "hammer"
             };
+
             newElement.setAttributes(attributes);
             expect(newElement.keys()).to.eql(Object.keys(attributes));
         });
     });
 
-    describe('findText()', function () {
-        it('should return the text of the first subelement', function () {
-            var SubElement = new Element('beautifulSubElement', {});
-            SubElement.setText('wonderful');
+    describe("findText()", () => {
+        it("should return the text of the first subelement", () => {
+            const SubElement = new Element("beautifulSubElement", {});
+
+            SubElement.setText("wonderful");
             newElement.addChildNode(SubElement);
-            expect(newElement.findText('beautifulSubElement')).to.eql('wonderful');
+            expect(newElement.findText("beautifulSubElement")).to.eql("wonderful");
         });
     });
 
-    describe('treeFind()', function () {
-        it('should return the first matching element', function () {
-            newElement.addChildNode(new Element('beautifulSubNode', {}));
-            newElement.find('beautifulSubNode').addChildNode(new Element('beautifulSubSubNode', {}));
-            var results = newElement.treeFind('beautifulSubSubNode');
+    describe("treeFind()", () => {
+        it("should return the first matching element", () => {
+            newElement.addChildNode(new Element("beautifulSubNode", {}));
+            newElement.find("beautifulSubNode").addChildNode(new Element("beautifulSubSubNode", {}));
+
+            const results = newElement.treeFind("beautifulSubSubNode");
+
             expect(results).to.be.instanceof(Element);
-            expect(results.name).to.eql('beautifulSubSubNode');
+            expect(results.name).to.eql("beautifulSubSubNode");
         });
     });
 });
--- test/lib/Parser.test.js
+++ test/lib/Parser.test.js
@@ -1,34 +1,32 @@
 "use strict";
 
-var expect = require('chai').expect;
+/*eslint-disable max-nested-callbacks */
 
-var Element = require('../../lib').Element;
-var Parser = require('../../lib').Parser;
-var xmlParser = new Parser();
+const expect = require("chai").expect;
 
-describe('Parser', function () {
+const Element = require("../../lib").Element;
+const Parser = require("../../lib").Parser;
+const xmlParser = new Parser();
 
-    describe('parse()', function () {
+describe("Parser", () => {
+    describe("parse()", () => {
+        it("should throw an error if an xml tag has a closed but no starting tag", () => {
+            const xmlString = "<opening></missing></opening>";
 
-        it('should throw an error if an xml tag has a closed but no starting tag', function () {
-            var xmlString = '<opening></missing></opening>';
             return xmlParser.parse(xmlString)
-                .catch(function (err) {
+                .catch((err) => {
                     expect(err).to.be.instanceof(Error);
                 });
         });
 
-        it('should create a proper js object', function () {
-            var xmlString = '<opening src="happening"><happening></happening></opening>';
-            var expectedObject = new Element('opening', {src: 'happening'}).addChildNode(new Element('happening')).root();
+        it("should create a proper js object", () => {
+            const xmlString = "<opening src=\"happening\"><happening></happening></opening>";
+            const expectedObject = new Element("opening", { src: "happening" }).addChildNode(new Element("happening")).root();
 
             return xmlParser.parse(xmlString)
-                .then(function (res) {
+                .then((res) => {
                     expect(res).to.eql(expectedObject);
                 });
-
-        })
-
+        });
     });
-
 });
